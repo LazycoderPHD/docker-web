b0012fb7207b58ea26fa46c2b8e77899f6497758
LazycoderPHD <phatdao2309@gmail.com>
2026-03-09 23:54:45 +0700

    Add project files.

---
+++ testing OOP bro code/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing_OOP_bro_code
+{
+    internal class class1
+    {
+        public class Testingcar
+        {
+            public string make = "Ford";
+            public string model = "Mustang";
+            public int year = 2026;
+            public double price = 670000.99;
+            public bool isRunning = false;
+
+            public void start()
+            {
+                isRunning = true;
+                Console.WriteLine("You start the engine!");
+            }
+
+            public void stop()
+            {
+                isRunning = false;
+                Console.WriteLine("You stop the engine!");
+            }
+
+            public void drive()
+            {
+                Console.WriteLine($"You drive the {model}");
+            }
+
+            public void brake()
+            {
+                Console.WriteLine($"You brake the {model}");
+            }
+        }
+    }
+}
---
+++ testing OOP bro code/Program.cs
@@ -0,0 +1,26 @@
+﻿using testing_OOP_bro_code;
+
+class1.Testingcar car1 = new class1.Testingcar();
+
+class1.Testingcar car2 = new class1.Testingcar();
+
+Console.WriteLine($"{car1.make} {car1.model}");
+Console.WriteLine($"{car2.make} {car2.model}");
+
+//car1.isRunning = false;
+
+//Console.WriteLine(car.make);
+//Console.WriteLine(car.model);
+//Console.WriteLine(car.year);
+//Console.WriteLine(car.price);
+//Console.WriteLine(car.isRunning);
+
+//Console.WriteLine(car1.isRunning);
+
+//car1.start();
+//Console.WriteLine(car1.isRunning);
+//car1.stop();
+//Console.WriteLine(car1.isRunning);
+
+//car1.drive();
+//car1.brake();
